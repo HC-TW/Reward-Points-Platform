cdac67e97813fc47308b81f3668caac868f4a8bf
HC-TW <jimmy12282@gmail.com>
2021-05-19 22:08:23 +0800

    replace timestamp into dueDate in loanRecord
    add option field in loanRecord

--- contracts/Credit.sol
+++ contracts/Credit.sol
@@ -9,14 +9,15 @@ contract Credit is Context {
         // address bank;
         uint256 loanAmount;
         uint256 pointsAmount;
-        uint256 timestamp;
+        uint256 dueDate;
         uint16 annualInterestRate; // This value should be divided by 1000. Ex: 3% = 30 / 1000
         uint256 loanBalance;
+        string option; // Loan case or something else
     }
 
     struct keyList {
         address[] keys;
-        mapping (address => bool) existing;
+        mapping(address => bool) existing;
     }
 
     address public _owner;
@@ -25,6 +26,16 @@ contract Credit is Context {
 
     mapping(address => mapping(address => loanRecord[])) _loanRecords;
     mapping(address => keyList) _bankToBorrowers;
+
+    event Loan(
+        address indexed borrower,
+        address indexed bank,
+        uint256 loanAmount,
+        uint256 pointsAmount,
+        uint256 dueDate,
+        uint16 annualInterestRate,
+        uint256 loanBalance
+    );
 
     modifier onlyOwner() {
         require(_msgSender() == _owner, "You are not a contract owner");
@@ -49,27 +60,31 @@ contract Credit is Context {
         address borrower,
         uint256 loanAmount,
         uint256 pointsAmount,
-        uint16 annualInterestRate
+        uint16 annualInterestRate,
+        string memory option
     ) public onlyBank {
+        uint256 dueDate = block.timestamp + 30 days;
         _loanRecords[_msgSender()][borrower].push() = loanRecord(
             loanAmount,
             pointsAmount,
-            block.timestamp,
+            dueDate,
             annualInterestRate,
-            loanAmount
+            loanAmount,
+            option
         );
-        if (!_bankToBorrowers[_msgSender()].existing[borrower])
-        {
+        if (!_bankToBorrowers[_msgSender()].existing[borrower]) {
             _bankToBorrowers[_msgSender()].keys.push(borrower);
             _bankToBorrowers[_msgSender()].existing[borrower] = true;
         }
         rp.deliver(borrower, pointsAmount);
+
+        emit Loan(borrower, _msgSender(), loanAmount, pointsAmount, dueDate, annualInterestRate, loanAmount);
     }
 
     function changeLoanLender(address oldBank, address newBank) public {
         require(_msgSender() == _owner || _msgSender() == _BankLiabilityAddr);
         address[] memory keys = _bankToBorrowers[oldBank].keys;
-        for (uint i = 0; i < keys.length; i++) {
+        for (uint256 i = 0; i < keys.length; i++) {
             _loanRecords[newBank][keys[i]] = _loanRecords[oldBank][keys[i]];
             delete _loanRecords[oldBank][keys[i]];
         }
@@ -79,5 +94,6 @@ contract Credit is Context {
 
 contract C_RPToken {
     function _banks(address) public view returns (bool) {}
+
     function deliver(address, uint256) public returns (bool) {}
 }
